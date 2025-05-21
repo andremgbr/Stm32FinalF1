ab896500c46f839ad402fbccf1bc5d48f023de09
André Roetger <64928582+andremgbr@users.noreply.github.com>
2025-05-21 14:42:50 -0300

    Added files

---
+++ Core/Inc/app.h
@@ -0,0 +1,18 @@
+#ifndef H_APP
+#define H_APP
+
+#include <stdint.h>
+#include "mcal.h"
+
+#define S_ON 0x01U
+#define S_OFF 0x00U
+
+//uint8_t application_init(void);
+uint8_t hazard_lights_blink(void);
+//uint8_t read_input(void);
+uint8_t monitor_read_can(void);
+uint8_t monitor_tcu(void);
+uint8_t check_can_communication(void);
+uint8_t handle_frame_can(can_frame *frame);
+
+#endif
